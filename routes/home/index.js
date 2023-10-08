5df3b3694bfff2236bef65437622ab15300dd1e0
오승훈 <onigiri282@gmail.com>
2023-10-08 20:50:36 +0900

    깃 저장소 초기화

---
+++ routes/home/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+// "/" 라우트
+router.get("/", ctrl.hello);
+
+// "/login" 라우트
+router.get("/login", ctrl.login);
+
+module.exports = router;
