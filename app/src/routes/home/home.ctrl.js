68917ba2bd3ccc1f2c1a8ce897eca3c59410459e
오승훈 <onigiri282@gmail.com>
2023-10-09 04:30:00 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");  //gpt code
 
 const output = {
     hello: (req, res)=> {
@@ -14,22 +15,8 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-        
-       const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하엿씁니다."
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
@@ -39,5 +26,4 @@ const process = {
 module.exports = {
     output,
     process,
-
 };
